1e7229a4881dcbef06f58aed902ad8d14a0a2c73
Sourov ahmad <mdsourovahmad19@gmail.com>
2021-04-22 22:52:18 +0600

    json data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 
 import './App.css';
+import Data from './Data/HomepageContent';
 
  
 function App() {
+
+ 
+
   return (
     <div> 
-
-
+   
+     {console.log(Data.map(content => (
+       <div className="mydata">
+         {content}
+       </div>
+     )
+      
+      
+      
+      ))}
+         
       
     </div>
   );
